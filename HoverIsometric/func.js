253f3971e624d8b7e4e52890ae1c0062a8b096d9
keven <r.kevensantos7@gmail.com>
2022-03-14 11:30:42 -0400

    add soud effects

--- HoverIsometric/func.js
+++ HoverIsometric/func.js
@@ -1,28 +1,25 @@
 window.onload = function(){
     var audio = document.getElementById('heartbeat')
     var target = document.querySelectorAll('a') 
+     var validS =  0;
    
     for(let i=0; i < target.length; i++){
-       target[i].addEventListener('mouseover', function() {
-           [].forEach.call(audio, function(audio) {
-             // do whatever
-             audio.play();
-             console.log('teste')
-           });
-           }, false);
-   
            target[i].addEventListener('mouseover', function() {
-               audio.play();
-           }, false);
+             validS++          
+            if(validS <= 1){audio.play()}
+
+             
+           });
    
            target[i].addEventListener('mouseleave', function() {
+               validS = 0
                audio.pause();
                audio.currentTime = 0;
-             }, false);
+             });
            
     }
     
-    console.log('teste')
+
 }
 
 //  target.addEventListenner('click', function(){
