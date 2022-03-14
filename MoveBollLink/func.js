fbbdebf213d12aeec2ccc8bae559feba62248947
keven <r.kevensantos7@gmail.com>
2022-03-14 19:59:53 -0400

    add glassHover e MoveBOllLink, finalizados 90%

---
+++ MoveBollLink/func.js
@@ -0,0 +1,17 @@
+const target = document.getElementsByClassName('btn')
+
+target[0].addEventListener('mousemove', function(e){
+    let x = e.pageX - target[0].offsetLeft
+    let y = e.pageY - target[0].offsetTop
+
+    target[0].style.setProperty('--x', x + 'px')
+    target[0].style.setProperty('--y', y + 'px')
+})
+
+target[1].addEventListener('mousemove', function(e){
+    let x = e.pageX - target[1].offsetLeft
+    let y = e.pageY - target[1].offsetTop
+
+    target[1].style.setProperty('--x', x + 'px')
+    target[1].style.setProperty('--y', y + 'px')
+})
