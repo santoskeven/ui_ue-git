308d7fb593a220529c22484d165e55239c105bc5
keven <r.kevensantos7@gmail.com>
2022-03-21 13:26:55 -0400

    add. botReaction(90%) e modificaçãoes em visibleMove and GlassHover

---
+++ botReaction/func.js
@@ -0,0 +1,47 @@
+const cont = document.getElementsByClassName('bot_one')
+    let target = document.querySelectorAll('.eye_c')
+
+    for(let i = 0; i < cont.length; i++){
+        cont[i].addEventListener('mousemove', function(e){
+
+            CapMove(e)
+        
+        })
+    }
+
+
+
+function CapMove(e){
+ 
+ for(let i = 0; i < target.length; i++){
+
+    let x = (target[i].getBoundingClientRect().left) + target[i].clientWidth / 2;
+    let y = (target[i].getBoundingClientRect().top) + target[i].clientHeight / 2;
+
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rot = (radian * (180 / Math.PI) * -1) - 125;
+    
+    target[i].style.transform = "rotate("+ rot + "deg)";
+ }
+
+}
+
+
+const head = document.getElementsByClassName('heir')
+const mouth = document.getElementsByClassName('mouth')
+
+ for(let i = 0; i < head.length; i++){
+
+    head[i].addEventListener('mouseenter', function(){
+        mouth[i].style.borderBottomLeftRadius = '1rem';
+        mouth[i].style.borderBottomRightRadius = '1rem';
+        mouth[i].style.height = '1.3rem';
+    })
+    
+    head[i].addEventListener('mouseleave', function(){
+        mouth[i].style.borderBottomLeftRadius = '8rem';
+        mouth[i].style.borderBottomRightRadius = '8rem';
+        mouth[i].style.height = '1.5rem';
+    })
+
+ }
